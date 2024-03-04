8e78d0c07b55e7e59565773342a8d6f3087b4966
imcatag <imcatag@gmail.com>
2024-03-04 20:54:36 +0200

    ADHD mode

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -17,7 +17,7 @@ public enum Tetromino
 public struct TetrominoData
 {
     public Tetromino tetromino;
-    public Tile tile;
+    public AnimatedTile tile;
     public Vector2Int[] cells { get; private set; }
     public void Initialize()
     {
